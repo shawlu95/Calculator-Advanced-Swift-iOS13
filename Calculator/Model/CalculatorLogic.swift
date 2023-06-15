6b59a46f8c55fc59456d7b327fc21a76cb4ff1e7
Shaw Lu <shawlu95@gmail.com>
2023-06-15 02:33:15 -0700

    intermediate calculation using tuple

--- Calculator/Model/CalculatorLogic.swift
+++ Calculator/Model/CalculatorLogic.swift
@@ -12,11 +12,13 @@ struct CalculatorLogic {
         
     private var number: Double? // global var should be private by default
     
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
     mutating func setNumber(_ number: Double) {
         self.number = number
     }
     
-    func calculate(symbol: String) -> Double? {
+    mutating func calculate(symbol: String) -> Double? {
         if let n = number {
             if symbol == "+/-" {
                 return n * -1
@@ -24,8 +26,30 @@ struct CalculatorLogic {
                 return 0
             } else if symbol == "%" {
                 return n / 100
+            } else if symbol == "=" {
+                return performTwoNumberCalculation(n2: n)
+            } else {
+                intermediateCalculation = (n1: n, calcMethod: symbol)
             }
         }
         return nil // deal with optional on client side
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double {
+        if let n1 = intermediateCalculation?.n1, let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "*":
+                return n1 * n2
+            case "/":
+                return n1 / n2
+            default:
+                fatalError("The operation passed in does not match any of the cases.")
+            }
+        }
+        return nil
     }
 }
