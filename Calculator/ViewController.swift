182bf083d0050277634184804c3e3b7ea388d59e
Shaw Lu <shawlu95@gmail.com>
2023-06-15 01:42:28 -0700

    handle +/-. AC, %

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,6 +16,20 @@ class ViewController: UIViewController {
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         // start accumulating a new number
         isFinishedTypingNumber = true
+        
+        guard let number = Double(displayLabel.text!) else {
+            fatalError("Cannot convert display label text to a Double.")
+        }
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
     }
 
     
